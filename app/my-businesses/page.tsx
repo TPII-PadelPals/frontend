657315ad3a5242aac42d27cc1848e8af0ced840e
Amaru Durán <amaruduran@gmail.com>
2025-04-11 17:36:16 -0300

    feat: add form to create into my-businesses page

--- app/my-businesses/page.tsx
+++ app/my-businesses/page.tsx
@@ -1,14 +1,63 @@
+'use client'
+
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+
+import { toast } from "@/hooks/use-toast";
+import { BusinessAddForm, BusinessFormValues } from "@/components/BusinessAddForm";
+
 
 export default function MyBusinessesPage() {
+
+
+
+  async function onSubmit(values: BusinessFormValues) {
+    console.log(values)
+    // const APP_PORT: string  = '8002'
+    // const APP_URL: string = `http://localhost`
+    // const response = await fetch(`${APP_URL}:${APP_PORT}/api/v1/businesses`, {
+    //     method: "POST",
+    //     headers: {
+    //         "Content-Type": "application/json",
+    //     },
+    //     body: JSON.stringify({ values }),
+    // });
+
+    // if (response.ok) {
+    //     toast({ description: "Establecimiento Creado" });
+    // } else {
+    //     toast({ variant: "destructive", description: "Ocurrió un error. Intente de nuevo más tarde." });
+    // }
+    toast({
+      variant: "success",
+      description:"Establecimiento Creado."
+    })
+}
+
   return (
     <div className="p-6 w-[80vw] mx-auto">
       <div className="flex flex-col gap-y-3.5">
         <div className="flex justify-between items-center ">
           <h1 className="text-2xl font-bold">Mis Establecimientos</h1>
-          <Button className="font-bold">+ Crear Establecimiento</Button>
+          <Dialog>
+            <DialogTrigger asChild>
+            <Button className="font-bold">+ Crear Establecimiento</Button>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[425px]">
+              <DialogHeader>
+                <DialogTitle>Crear Establecimiento</DialogTitle>
+              </DialogHeader>
+              <BusinessAddForm onSubmit={onSubmit}/>
+            </DialogContent>
+          </Dialog>
         </div>
         <Separator></Separator>
       </div>
