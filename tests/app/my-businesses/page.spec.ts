33ad7625604a969b7f8c4562a7ee92b1bff64d23
Amaru Durán <amaruduran@gmail.com>
2025-04-12 12:35:28 -0300

    test: my-business form tests

--- tests/app/my-businesses/page.spec.ts
+++ tests/app/my-businesses/page.spec.ts
@@ -8,3 +8,49 @@ test('should navigate to my-businesses page', async ({ page }) => {
 
   await expect(page.getByRole('button', { name: '+ Crear Establecimiento' })).toBeVisible();
 })
+
+test('should complete the create business form and it will get sucess mesagge', async ({ page }) => {
+
+  await page.goto('/my-businesses');
+
+  await page.getByRole('button', { name: '+ Crear Establecimiento' }).click()
+
+  await page.getByLabel('Nombre').fill('Padel Sports');
+  await page.getByLabel('Dirección').fill('Paseo Colon 850');
+
+  await page.getByRole('button', { name: 'Crear Establecimiento' }).click()
+
+  await expect(page.getByRole('dialog')).toBeHidden();
+
+  await expect(page.getByText('Establecimiento Creado.', { exact: true })).toBeVisible();
+});
+
+test('complete the create business form with short Name then will get error mesagge', async ({ page }) => {
+
+  await page.goto('/my-businesses');
+
+  await page.getByRole('button', { name: '+ Crear Establecimiento' }).click()
+
+  await page.getByLabel('Nombre').fill('Fi');
+  await page.getByLabel('Dirección').fill('Paseo Colon 850');
+
+  await page.getByRole('button', { name: 'Crear Establecimiento' }).click()
+
+  await expect(page.getByText('El mínimo de caracteres es 3.')).toBeVisible();
+});
+
+test('should be cancel the create business form and back to my-businesses page', async ({ page }) => {
+
+  await page.goto('/my-businesses');
+
+  await page.getByRole('button', { name: '+ Crear Establecimiento' }).click()
+
+  await page.getByLabel('Nombre').fill('Padel Sports');
+  await page.getByLabel('Dirección').fill('Paseo Colon 850');
+
+  await page.getByRole('button', { name: 'Cancelar' }).click()
+
+  await expect(page.getByRole('dialog')).toBeHidden();
+
+  await expect(page.getByRole('heading', {name: 'Mis Establecimientos'})).toBeVisible()
+});
