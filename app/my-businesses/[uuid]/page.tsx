06f42305e0c76dc65e1dff6c82b6033036ca9a26
amaruDuran <amaruduran@gmail.com>
2025-05-11 02:10:27 -0300

    feat: get courts endpoint fetch

--- app/my-businesses/[uuid]/page.tsx
+++ app/my-businesses/[uuid]/page.tsx
@@ -1,5 +1,13 @@
+import { Court, getCourts } from "@/app/services/business-service";
 import CourtsByBusiness from "./CourtsByBusiness";
 
 export default async function CourtsByBusinessPage() {
+  /* eslint-disable */
+  let _courts: Court[] = []
+  if (process.env.OWNER_ID && process.env.BUSINESS_PUBLIC_ID){
+    _courts = await getCourts(process.env.OWNER_ID, process.env.BUSINESS_PUBLIC_ID)
+  }
+  /* eslint-enable */
+
   return (<CourtsByBusiness/>)
 }
--- app/services/business-service.ts
+++ app/services/business-service.ts
@@ -7,8 +7,38 @@ export type Business = {
     location: string
 }
 
+export type Court = {
+    court_public_id: string
+    business_public_id: string
+    name: string
+    price_per_hour: string
+}
+
 export async function getData(owner_id: string): Promise<Business[]> {
   const response = await fetch(`http://${process.env.BUSINESS_SERVICE_URL}:${process.env.BUSINESS_SERVICE_PORT}/api/v1/businesses/?owner_id=${owner_id}`, {
+    method: "GET",
+    headers: {
+        "Content-Type": "application/json",
+        "x-api-key": `${process.env.BUSINESS_SERVICE_API_KEY}`,
+    },
+    cache: "no-store",
+  })
+
+  if (response.ok) {
+    const data_json = await response.json()
+    return data_json.data
+
+  } else {
+    console.log(response)
+    return []
+  }
+}
+
+export async function getCourts(owner_id: string, business_public_id: string): Promise<Court[]> {
+  const url = `http://${process.env.BUSINESS_SERVICE_URL}:${process.env.BUSINESS_SERVICE_PORT}/api/v1/padel-courts/` +
+              `?user_id=${owner_id}&business_public_id=${business_public_id}`
+
+  const response = await fetch(url, {
     method: "GET",
     headers: {
         "Content-Type": "application/json",
