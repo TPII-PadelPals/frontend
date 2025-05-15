88c24cac29c6b4939bb4939e8bc1425df0b7ee27
dima1997 <franki.dimaria@gmail.com>
2025-05-15 00:26:42 -0300

    feat: click on logo redirects to my-business if authenticated

--- components/AuthLayout.tsx
+++ components/AuthLayout.tsx
@@ -5,6 +5,7 @@ import { usePathname, useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 const publicPaths = [
+  "/",
   "/auth/log-in",
   "/auth/sign-up",
   "/auth/forgot-password",
@@ -40,7 +41,7 @@ export default function AuthLayout({
     <>
       <nav className="w-full top-0 left-0 flex justify-between items-center header">
         <div>
-          <Link href="/">
+          <Link href={authenticated ? "/my-businesses" : "/"}>
             <h1 className="text-4xl font-extrabold">PadelPals</h1>
           </Link>
         </div>
