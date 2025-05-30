a466dea1862107dea2786e0054c9c8462f4e95a3
Amaru Durán <amaruduran@gmail.com>
2025-05-30 20:20:52 -0300

    refactor: extract helper function for calendar

--- app/my-businesses/[uuid]/courts/[courtUuid]/page.tsx
+++ app/my-businesses/[uuid]/courts/[courtUuid]/page.tsx
@@ -14,6 +14,7 @@ import useCourtAvailabilityCreate from "@/hooks/businesses/useCourtAvailabilityC
 import useCourtsAvailabilityList from "@/hooks/businesses/useCourtAvailabilityList";
 import useMatchesList from "@/hooks/matches/useMatchesList";
 import { useToast } from "@/hooks/use-toast";
+import { formatToLocalISOString } from "@/lib/calendar/utils";
 import { CourtAvailability } from "@/types/businesses";
 import { DayPilot, DayPilotCalendar } from "@daypilot/daypilot-lite-react";
 import { useQueryClient } from "@tanstack/react-query";
@@ -107,15 +108,15 @@ const CourtAvailabilityPage = ({
       );
 
       let backColor = AVAILABLE_COLOR;
-      let status = "Disponible";
+      let displayStatus = "Disponible";
 
       if (match) {
         if (match.status === "Reserved") {
           backColor = RESERVED_COLOR; // Rojo
-          status = "Ocupado";
+          displayStatus = "Ocupado";
         } else if (match.status === "Provisional") {
           backColor = PROVISIONAL_COLOR; // Amarillo
-          status = "Provisional";
+          displayStatus = "Provisional";
         }
       }
 
@@ -129,24 +130,13 @@ const CourtAvailabilityPage = ({
       const endDateObject = new Date(startDateObject);
       endDateObject.setHours(startDateObject.getHours() + 1);
 
-      // Helper to format Date object to 'yyyy-MM-ddTHH:mm:ss' local time string
-      const formatToLocalISOString = (date: Date) => {
-        const year = date.getFullYear();
-        const month = String(date.getMonth() + 1).padStart(2, "0");
-        const day = String(date.getDate()).padStart(2, "0");
-        const hours = String(date.getHours()).padStart(2, "0");
-        const minutes = String(date.getMinutes()).padStart(2, "0");
-        const seconds = String(date.getSeconds()).padStart(2, "0");
-        return `${year}-${month}-${day}T${hours}:${minutes}:${seconds}`;
-      };
-
       const displayHour = String(startDateObject.getHours()).padStart(2, "0");
       const displayMinutes = String(startDateObject.getMinutes()).padStart(
         2,
         "0"
       );
 
-      let text = `${displayHour}:${displayMinutes} - ${status}`;
+      const text = `${displayHour}:${displayMinutes} - ${displayStatus}`;
 
       return {
         start: formatToLocalISOString(startDateObject),
